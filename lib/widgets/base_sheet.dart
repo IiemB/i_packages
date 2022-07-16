d0f184e82c8bfa633a8db1ed5ce1eb758d4f1f23
IiemB <iiem.ahmad@ukur.com>
2022-07-16 15:26:09 +0700

    update

--- lib/widgets/base_sheet.dart
+++ lib/widgets/base_sheet.dart
@@ -3,43 +3,71 @@ import 'package:flutter/material.dart';
 import 'widgets.dart';
 
 class IBaseSheet extends StatelessWidget {
-  final Widget title;
+  final Widget? title;
   final List<Widget> content;
   final Color? backgroundColor;
   final Future<bool> Function()? onWillPop;
+  final bool isDraggable;
   const IBaseSheet({
     Key? key,
-    required this.title,
+    this.title,
     required this.content,
     this.backgroundColor,
     this.onWillPop,
+    this.isDraggable = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: onWillPop,
-      child: DraggableScrollableSheet(
-        maxChildSize: 0.9,
-        expand: false,
-        snap: true,
-        builder: (context, scrollController) => Container(
-          color: backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            controller: scrollController,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Center(child: ITopBar()),
-                title,
-                const IGap(),
-                ...content
-              ],
+      child: isDraggable
+          ? DraggableScrollableSheet(
+              maxChildSize: 0.95,
+              expand: false,
+              snap: true,
+              builder: (context, scrollController) => Container(
+                color: backgroundColor ??
+                    Theme.of(context).scaffoldBackgroundColor,
+                padding: const EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Center(child: ITopBar()),
+                      if (title != null) ...[
+                        title!,
+                        const IGap(),
+                      ],
+                      ...content
+                    ],
+                  ),
+                ),
+              ),
+            )
+          : BottomSheet(
+              enableDrag: false,
+              onClosing: () {},
+              builder: (context) => Container(
+                color: backgroundColor ??
+                    Theme.of(context).scaffoldBackgroundColor,
+                padding: const EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Center(child: ITopBar()),
+                      if (title != null) ...[
+                        title!,
+                        const IGap(),
+                      ],
+                      ...content
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
